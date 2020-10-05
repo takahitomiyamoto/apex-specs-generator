bd1a50352d6cf176c616a728c6858c461fe58c66
Takahito Miyamoto <takahito0508+github@gmail.com>
2020-10-05 17:29:04 +0900

    [modify] :+1: header.js

--- scripts/gulp/tooling/doc/header.js
+++ scripts/gulp/tooling/doc/header.js
@@ -1,18 +1,25 @@
 /**
  * @name doc/header.js
  */
-import { TABLE_HEADER_HEADER, TABLE_HEADER_TRIGGER } from './config';
 import {
-  createCodeClass,
-  createTableClass,
-  createTableHeaderApexDoc
-} from './common';
+  REGEXP_HEADER_DESCRIPTION,
+  REGEXP_HEADER_OTHERS,
+  REGEXP_HEADER_OTHERS_KEY_PREFIX,
+  REGEXP_HEADER_OTHERS_KEY_SUFFIX,
+  REGEXP_HEADER_CLASS_SIGNATURE,
+  REGEXP_HEADER_TRIGGER_SIGNATURE,
+  REGEXP_HEADER_SIGNATURE_SUFFIX,
+  TABLE_HEADER_HEADER
+} from './config';
+import { fetchTag, fetchTagsHeader, fetchSignatureHeader } from './common';
+import { createHeaderAreaApexClass } from './apex-class';
+import { createHeaderAreaTrigger } from './apex-trigger';
 
 /**
- * @description createTableHeader
+ * @description _createTableHeader
  * @param {*} params
  */
-const createTableHeader = (params) => {
+const _createTableHeader = (params) => {
   return {
     headers: TABLE_HEADER_HEADER,
     rows: [
@@ -26,85 +33,56 @@ const createTableHeader = (params) => {
 };
 
 /**
- * @description createTableTrigger
- * @param {*} params
+ * @description _createTableRowsApexDoc
+ * @param {*} body
  */
-const createTableTrigger = (params) => {
+const _createTableRowsApexDoc = (body) => {
+  return [[`${body.header.description}`]];
+};
+
+/**
+ * @description _createListApexDoc
+ * @param {*} body
+ */
+const _createListApexDoc = (body) => {
+  const tags = body.header.others;
+  return tags.map((tag) => {
+    return `**\`${tag.key}\`** : ${tag.value}`;
+  });
+};
+
+/**
+ * @description _parseBodyHeader
+ * @param {*} body
+ * @param {*} regexp
+ */
+const _parseBodyHeader = (body, regexp) => {
+  body = body.replace(/\r\n/, /\n/);
+  const arrayBody = body.split(/\n/);
+
+  const description = fetchTag(arrayBody, regexp.regexpDescription);
+
+  const others = fetchTagsHeader({
+    body: arrayBody,
+    regexpValue: regexp.regexpOthers,
+    regexpKeyPrefix: regexp.regexpOthersKeyPrefix,
+    regexpKeySuffix: regexp.regexpOthersKeySuffix
+  });
+
+  const signature = fetchSignatureHeader({
+    body: arrayBody,
+    regexpValue: regexp.regexpSignature,
+    regexpKeyPrefix: '',
+    regexpKeySuffix: regexp.regexpSignatureSuffix
+  });
+
   return {
-    headers: TABLE_HEADER_TRIGGER,
-    rows: [
-      [
-        `${params.usageBeforeInsert ? 'Y' : ''}`,
-        `${params.usageBeforeUpdate ? 'Y' : ''}`,
-        `${params.usageBeforeDelete ? 'Y' : ''}`,
-        `${params.usageAfterInsert ? 'Y' : ''}`,
-        `${params.usageAfterUpdate ? 'Y' : ''}`,
-        `${params.usageAfterDelete ? 'Y' : ''}`,
-        `${params.usageAfterUndelete ? 'Y' : ''}`
-      ]
-    ]
-  };
-};
-
-/**
- * @description createCodeContentTrigger
- * @param {*} params
- */
-const createCodeContentTrigger = (params) => {
-  return [`trigger ${params.name} on ${params.entityDefinition.DeveloperName}`];
-};
-
-/**
- * @description createCodeTrigger
- * @param {*} params
- */
-const createCodeTrigger = (params) => {
-  return {
-    language: 'java',
-    content: createCodeContentTrigger(params)
-  };
-};
-
-/**
- * @description createHeaderAreaApexClass
- * @param {*} params
- */
-const createHeaderAreaApexClass = (params) => {
-  return [
-    {
-      table: createTableHeader(params)
-    },
-    {
-      table: createTableClass(params)
-    },
-    {
-      table: createTableHeaderApexDoc(params)
-    },
-    {
-      code: createCodeClass(params)
+    header: {
+      description: description,
+      others: others,
+      signature: signature
     }
-  ];
-};
-
-/**
- * @description createHeaderAreaTrigger
- * @param {*} params
- */
-const createHeaderAreaTrigger = (params) => {
-  return [
-    {
-      table: createTableHeader(params)
-    },
-    {
-      table: createTableTrigger(params)
-    },
-    {
-      table: createTableHeaderApexDoc(params)
-    },
-    {
-      code: createCodeTrigger(params)
-    }
-  ];
+  };
 };
 
 /**
@@ -114,12 +92,12 @@ const createHeaderAreaTrigger = (params) => {
 export const createHeaderArea = (params) => {
   const apex = params.apex;
   const apexMember = params.apexMember;
+  const body = params.body;
   // Common
   const namespace = apexMember.namespace;
   const manageableState = apex.ManageableState;
   const apiVersion = apex.ApiVersion.toFixed(1);
   const attributes = apex.attributes;
-  const body = apex.Body;
   const table = apexMember.tableDeclaration;
   const name = table.name;
   // ApexClass
@@ -139,34 +117,67 @@ export const createHeaderArea = (params) => {
 
   switch (attributes.type) {
     case 'ApexClass':
-      return createHeaderAreaApexClass({
-        namespace: namespace,
-        manageableState: manageableState,
-        apiVersion: apiVersion,
-        annotations: annotations,
-        modifiers: modifiers,
-        name: name,
-        parentClass: parentClass,
-        interfaces: interfaces,
-        body: body
-      });
+      return createHeaderAreaApexClass(
+        {
+          namespace: namespace,
+          manageableState: manageableState,
+          apiVersion: apiVersion,
+          annotations: annotations,
+          modifiers: modifiers,
+          name: name,
+          parentClass: parentClass,
+          interfaces: interfaces,
+          body: body
+        },
+        {
+          createTableHeader: _createTableHeader,
+          createTableRowsApexDoc: _createTableRowsApexDoc,
+          createListApexDoc: _createListApexDoc
+        }
+      );
     case 'ApexTrigger':
-      return createHeaderAreaTrigger({
-        namespace: namespace,
-        manageableState: manageableState,
-        apiVersion: apiVersion,
-        usageBeforeInsert: usageBeforeInsert,
-        usageBeforeUpdate: usageBeforeUpdate,
-        usageBeforeDelete: usageBeforeDelete,
-        usageAfterInsert: usageAfterInsert,
-        usageAfterUpdate: usageAfterUpdate,
-        usageAfterDelete: usageAfterDelete,
-        usageAfterUndelete: usageAfterUndelete,
-        name: name,
-        entityDefinition: entityDefinition,
-        body: body
-      });
+      return createHeaderAreaTrigger(
+        {
+          namespace: namespace,
+          manageableState: manageableState,
+          apiVersion: apiVersion,
+          usageBeforeInsert: usageBeforeInsert,
+          usageBeforeUpdate: usageBeforeUpdate,
+          usageBeforeDelete: usageBeforeDelete,
+          usageAfterInsert: usageAfterInsert,
+          usageAfterUpdate: usageAfterUpdate,
+          usageAfterDelete: usageAfterDelete,
+          usageAfterUndelete: usageAfterUndelete,
+          name: name,
+          entityDefinition: entityDefinition,
+          body: body
+        },
+        {
+          createTableHeader: _createTableHeader,
+          createTableRowsApexDoc: _createTableRowsApexDoc,
+          createListApexDoc: _createListApexDoc
+        }
+      );
     default:
       return {};
   }
+};
+
+/**
+ * @description parseBodyHeader
+ * @param {*} body
+ * @param {*} type
+ */
+export const parseBodyHeader = (body, type) => {
+  return _parseBodyHeader(body, {
+    regexpDescription: REGEXP_HEADER_DESCRIPTION,
+    regexpOthers: REGEXP_HEADER_OTHERS,
+    regexpOthersKeyPrefix: REGEXP_HEADER_OTHERS_KEY_PREFIX,
+    regexpOthersKeySuffix: REGEXP_HEADER_OTHERS_KEY_SUFFIX,
+    regexpSignature:
+      'ApexClass' === type
+        ? REGEXP_HEADER_CLASS_SIGNATURE
+        : REGEXP_HEADER_TRIGGER_SIGNATURE,
+    regexpSignatureSuffix: REGEXP_HEADER_SIGNATURE_SUFFIX
+  });
 };
