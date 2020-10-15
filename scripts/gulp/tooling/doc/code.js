54fd14db6f8efcff2d3bf9f631c7ce0b05879235
Takahito Miyamoto <takahito0508+github@gmail.com>
2020-10-15 20:20:49 +0900

    [modify] doc/code.js

---
+++ scripts/gulp/tooling/doc/code.js
@@ -0,0 +1,32 @@
+/**
+ * @name doc/code.js
+ */
+import { convertSignature } from './config';
+
+/**
+ * @description _createCodeContent
+ * @param {*} item
+ */
+const _createCodeContent = (item) => {
+  const content = [];
+  const signature = convertSignature(item.signature);
+  content.push(signature);
+  return content;
+};
+
+/**
+ * @description createCode
+ * @param {*} params
+ * @param {*} item
+ * @param {*} _createCodeContent
+ */
+export const createCode = (item) => {
+  return !item
+    ? {}
+    : {
+        code: {
+          language: 'java',
+          content: _createCodeContent(item)
+        }
+      };
+};
